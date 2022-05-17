67d56e38b24f42c1f3bc15b4764034db823054fa
yuvalnahum1 <101082432+yuvalnahum1@users.noreply.github.com>
2022-05-17 17:10:56 +0300

    Sixth

---
+++ GameEngine6/src/engineTester/MainGameLoop.java
@@ -0,0 +1,63 @@
+package engineTester;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import models.RawModel;
+import models.TexturedModel;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.MasterRenderer;
+import renderEngine.OBJLoader;
+import terrains.Terrain;
+import textures.ModelTexture;
+import entities.Camera;
+import entities.Entity;
+import entities.Light;
+import entities.Player;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		
+		
+		RawModel model = OBJLoader.loadObjModel("tree", loader); 
+		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("tree")));
+		ModelTexture texture = staticModel.getTexture();
+		Player player =  new Player(staticModel, new Vector3f(80,0,70),0,0,0,20); //20 represents the size of entity
+		
+		Light light = new Light(new Vector3f(3000,2000,2000),new Vector3f(1,1,1));
+		
+		Terrain terrain = new Terrain(0,0,loader,new ModelTexture(loader.loadTexture("mon")));
+		//Terrain terrain2 = new Terrain(1,0,loader,new ModelTexture(loader.loadTexture("mon")));
+		
+		Camera camera = new Camera();	
+		MasterRenderer renderer = new MasterRenderer();
+		
+		while(!Display.isCloseRequested()){
+			//renderer.processEntity(entity);
+			//entity.increaseRotation(0, 1, 0);
+			camera.move();
+			player.move();
+			renderer.processEntity(player);
+			renderer.processTerrain(terrain);
+			//renderer.processTerrain(terrain2);
+			renderer.render(light, camera);
+			DisplayManager.updateDisplay();
+		}
+
+		renderer.cleanUp();
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+
+	}
+
+}
